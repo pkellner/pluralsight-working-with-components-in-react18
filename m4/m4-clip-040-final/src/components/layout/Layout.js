859af8563936445a435cdcb6444faf231fd01f85
pkellner <peter@peterkellner.net>
2022-12-27 16:21:25 -0800

    wip

---
+++ m4/m4-clip-040-final/src/components/layout/Layout.js
@@ -0,0 +1,18 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import { useContext } from "react";
+import { ThemeContext } from "../../App";
+
+const Layout = (props) => {
+  const { darkTheme } = useContext(ThemeContext);
+  const layoutVersion = "Layout Version 2.0";
+  return (
+    <div className="container" data-theme={darkTheme ? "dark" : "light"}>
+      <Header {...props} layoutVersion={layoutVersion} />
+      {props.children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
