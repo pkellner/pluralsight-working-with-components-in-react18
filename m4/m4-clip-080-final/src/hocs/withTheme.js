e2cccf9d5413860242bf9784e7590e3f45873e09
pkellner <peter@peterkellner.net>
2022-12-27 17:12:05 -0800

    done m4 context and hocs

---
+++ m4/m4-clip-080-final/src/hocs/withTheme.js
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+
+export const withTheme = (Component) => (props) => {
+  const { darkTheme, toggleTheme } = useContext(ThemeContext);
+  return (
+    <Component {...props} darkTheme={darkTheme} toggleTheme={toggleTheme} />
+  );
+};
