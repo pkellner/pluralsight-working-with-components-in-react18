a895a10c3a2a433cef3cea228c177350401fe641
pkellner <peter@peterkellner.net>
2022-12-27 16:39:13 -0800

    wipwip

---
+++ m4/m4-clip-050-final/src/contexts/ThemeContext.js
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext({});
+
+export const ThemeProvider = ({ children }) => {
+  const [darkTheme, setDarkTheme] = useState(false);
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ darkTheme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
