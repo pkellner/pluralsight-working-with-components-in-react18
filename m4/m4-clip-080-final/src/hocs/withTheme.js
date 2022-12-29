4a1c24aff94c92ab52565dd4639adafa883ba18d
pkellner <peter@peterkellner.net>
2022-12-29 15:08:06 -0800

    wip

--- m4/m4-clip-080-final/src/hocs/withTheme.js
+++ m4/m4-clip-080-final/src/hocs/withTheme.js
@@ -1,41 +1,11 @@
 import { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-
-
-export const withTheme = (WrappedComponent) => {
-  return Func;
+export const withTheme = (Component) => {
   function Func(props) {
     const ctx = useContext(ThemeContext)
-    return <WrappedComponent {...props} darkTheme={ctx.darkTheme} toggleTheme={ctx.toggleTheme}/>
+    return <Component {...props} darkTheme={ctx.darkTheme} toggleTheme={ctx.toggleTheme}/>
   }
+  return Func;
+  
 }
-
-// // works but with named eslint error
-// export const withTheme = (WrappedComponent) => {
-//   return function (props) {
-//     const ctx = useContext(ThemeContext)
-//     return <WrappedComponent {...props} darkTheme={ctx.darkTheme} toggleTheme={ctx.toggleTheme}/>
-//   }
-// }
-
-// import { useContext } from "react";
-// import { ThemeContext } from "../contexts/ThemeContext";
-//
-// function InnerComponent(props) {
-//   const Component = props.Component;
-//   const { darkTheme, toggleTheme } = useContext(ThemeContext);
-//   return (
-//     <Component
-//       {...props.props}
-//       darkTheme={darkTheme}
-//       toggleTheme={toggleTheme}
-//     />
-//   );
-// }
-//
-// export const withTheme = (Component) => {
-//   return function f(props) {
-//     return <InnerComponent props={props} Component={Component} />;
-//   };
-// };
