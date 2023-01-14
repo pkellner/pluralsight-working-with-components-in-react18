f131f41e2f53a151e783c0150c350eb841ee8582
pkellner <peter@peterkellner.net>
2023-01-13 16:13:35 -0800

    wip

--- m5/m5-clip-050/src/hooks/useTheme.js
+++ m5/m5-clip-050/src/hooks/useTheme.js
@@ -5,9 +5,17 @@ export default function useTheme() {
   const toggleTheme = () => {
     setDarkTheme(!darkTheme);
   };
-  useDebugValue(`Theme:${darkTheme ? "dark" : "light"}`);
+  useDebugValue(
+    `Theme:${darkTheme ? "dark" : "light"}`,
+    (val) => {
+      if (val === "...") {
+        return "Resut ...";
+      } else {
+        return "Other Result...";
+      }
+    }
+  );
   return {
-    darkTheme,
-    toggleTheme,
-  };
+    darkTheme, toggleTheme,
+  }
 }
