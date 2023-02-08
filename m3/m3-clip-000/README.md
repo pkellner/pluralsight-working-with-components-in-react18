94785495b48c928fe0aa536aa542986dce0346af
pkellner <peter@peterkellner.net>
2023-02-07 17:39:43 -0800

    Code update for module 3, the starting code

---
+++ m3/m3-clip-000/README.md
@@ -0,0 +1,8 @@
+# Module 3: Working with Components in React 18
+
+Author: Peter Kellner
+
+This module is all about the starting code that will be used going
+forward in this course. The reason there is only one clip listed (m3-clip-000) 
+is that no code is changed through all the clips.  This also is the same as m4-clip-000 meaning
+this code is the starting point for module 4, Global State Sharing and React Context
---
+++ m4/m4-clip-000/src/App.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import ToDoListWithToolbar from "./components/todo/ToDoListWithToolbar";
+import { TodosDataProvider } from "./contexts/ToDosDataContext";
+import ToDoManager from "./components/todo/ToDoManager";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Layout from "./components/layout/Layout";
+
+const App = () => {
+  const [displayStatus, setDisplayStatus] = useState("all"); // all, pending, completed
+  const [important, setImportant] = useState(false);
+  const [searchText, setSearchText] = useState("");
+
+  const [darkTheme, setDarkTheme] = useState(false);
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  return (
+    <TodosDataProvider>
+      <Layout toggleTheme={toggleTheme} darkTheme={darkTheme}>
+        <ToDoListWithToolbar
+          displayStatus={displayStatus}
+          setDisplayStatus={setDisplayStatus}
+          import={important}
+          setImportant={setImportant}
+          searchText={searchText}
+          setSearchText={setSearchText}
+        >
+          <ToDoManager
+            displayStatus={displayStatus}
+            important={important}
+            searchText={searchText}
+            darkTheme={darkTheme}
+          />
+        </ToDoListWithToolbar>
+      </Layout>
+    </TodosDataProvider>
+  );
+};
+
+export default App;
